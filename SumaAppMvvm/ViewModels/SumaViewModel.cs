52b4508d7e1574be432070e28fe1521a551241e9
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-09-02 16:02:09 -0600

    Creando el viewModel

---
+++ SumaAppMvvm/ViewModels/SumaViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SumaAppMvvm.ViewModels
+{
+    public partial class SumaViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double a;
+
+        [ObservableProperty]
+        private double b;
+        
+
+        private double result;
+
+        private void Alerta(string Título, string Mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Título, Mensaje, "Aceptar"));
+        }
+        [RelayCommand]
+        private void calcular()
+        {
+            if (A == 0 || B == 0)
+            {
+                Alerta("Advertencia", "Valor de A o B no puede ser cero.");
+            }
+            else 
+            {
+                result = a + b;
+
+            }
+        }
+    }
+}
+
+
---
+++ SumaAppMvvm/Views/SumaView.xaml.cs
@@ -0,0 +1,9 @@
+namespace SumaAppMvvm.Views;
+
+public partial class SumaView : ContentPage
+{
+	public SumaView()
+	{
+		InitializeComponent();
+	}
+}
